951486490c08e6dfa68dc141d89c83b9fc153aef
Max Fan Wai Pio <maxfan@ad.sport.gov.mo>
2020-03-31 19:38:53 +0800

    added table

--- index.php
+++ index.php
@@ -6,10 +6,22 @@
 		
 		<!-- js datatable -->
 		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+		
 		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+		<!--
+		<script src="js/jquery/external/jquery/jquery.js"></script>
+		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/js/foundation.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.css">-->
 		<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 		<link rel="stylesheet" href="css/main.css">
+		
 		<title>Sport Ticket</title>
+		<script>
+			// handle form
+		</script>
 	</head>
 	
 	<body>
@@ -20,7 +32,7 @@
 				$siteName = get_sitename();
 				echo $siteName; 
 			?>
-			<button>新增</button>
+			<button id="create-ticket">新增</button>
 		  </div>
 		</header>
 
@@ -28,6 +40,21 @@
 		  <div class="row">
 			<ul>
 			</ul>
+			<div id="dialog-form" title="新增" style="display:none">
+			  <form>
+				<fieldset>
+				  <label for="name">Name</label>
+				  <input type="text" name="name" id="name" value="Jane Smith" class="text ui-widget-content ui-corner-all">
+				  <label for="email">Email</label>
+				  <input type="text" name="email" id="email" value="jane@smith.com" class="text ui-widget-content ui-corner-all">
+				  <label for="password">Password</label>
+				  <input type="password" name="password" id="password" value="xxxxxxx" class="text ui-widget-content ui-corner-all">
+			 
+				  <!-- Allow form submission with keyboard without duplicating the dialog button -->
+				  <input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
+				</fieldset>
+			  </form>
+			</div>
 		  </div>
 		</nav>
 
