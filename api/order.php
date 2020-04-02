ea19122da4b92cd53d1e78d05d956836bdd4df0f
Max Fan Wai Pio <maxfan@ad.sport.gov.mo>
2020-04-02 10:07:05 +0800

    change site

--- api/order.php
+++ api/order.php
@@ -1,14 +1,19 @@
 <?php
 
 include_once('../config.php');
+include_once(ROOT_PATH . 'global.php');
 include_once(ROOT_PATH . 'dal\order_view.php'); 
+
 $queries = array();
 parse_str($_SERVER['QUERY_STRING'], $queries);
+
 $date = date($queries['date']);
 $view = new OrderView();
 $result = array();
 
-$list = $view->get_order_list($date);
+$sid = (new GlobalCommon())->get_siteid();
+
+$list = $view->get_order_list($date, $sid);
 
 $result['data'] = $list;
 
--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 	$config_db_user = "sportticket";
 	$config_db_pw = "password!";
 	$config_db_name = "sport_ticket";
-	$config_siteid = 1;	
+	$config_siteid = 2;	
 	
 	define('ROOT_PATH', dirname(__DIR__) . '/sportticket/');
 ?>
