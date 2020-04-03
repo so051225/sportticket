c057428856899e943495e7d0746c2f26f3a209e4
Lo chon wai <lochonwai@ad.sport.gov.mo>
2020-04-03 15:28:22 +0800

    use local js and css file

--- order_form.php
+++ order_form.php
@@ -4,11 +4,9 @@
 	<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
 		
-		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-		<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+		<script src="js/jquery-3.3.1.js"></script>
 		<link rel="stylesheet" href="css/main.css">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">		
 		
 		<title>Order Form</title>
 
