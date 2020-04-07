e5c22f307d4454cce802a5affa20d05306dc0470
Max Fan Wai Pio <maxfan@ad.sport.gov.mo>
2020-04-07 09:34:42 +0800

    fix comment

--- config.php
+++ config.php
@@ -4,14 +4,16 @@
 	$config_db_user = "sportticket";
 	$config_db_pw = "password!";
 	$config_db_name = "sport_ticket";
-	$config_siteid = 1;	
+	$config_siteid = 4;	
 	
 	/*
-		1 	奧林匹克體育中心(羽毛球場) OC 	Centro Desportivo Olimpico
-		2 	何東中葡小學綜合運動室 	Ginasio Polidesportivo da Escola Primaria Oficial ...
-		3 	氹仔東北體育中心 	Centro Desportivo do Nordeste da Taipa
-		4 	澳門東亞運動會體育館 	Nave Desportiva dos Jogos da Ásia Oriental de Maca...
-		5 	澳大綜合體育館 	Complexo Desportivo da UM
+
+	1 	澳門東亞運動會體育館 	Nave Desportiva dos Jogos da Ásia Oriental de Maca...
+	2 	氹仔東北體育中心 	Centro Desportivo do Nordeste da Taipa
+	3 	奧林匹克體育中心(羽毛球場) OC 	Centro Desportivo Olimpico
+	4 	何東中葡小學綜合運動室 	Ginasio Polidesportivo da Escola Primaria Oficial ...
+	5 	澳大綜合體育館 	Complexo Desportivo da UM
+		
 	*/
 	
 	define('ROOT_PATH', dirname(__DIR__) . '/sportticket/');
