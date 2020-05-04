582410b7c9c6964c2ba6cb693e9d5583ec0bfa60
Lo chon wai <lochonwai@ad.sport.gov.mo>
2020-05-04 16:46:58 +0800

    增加使用場地數據

--- db/sport-ticket-DATA.sql
+++ db/sport-ticket-DATA.sql
@@ -13,28 +13,42 @@ INSERT INTO sport_ticket.tb_site (sid, site_name_zh, site_name_pt) VALUES ('5', 
 
 #--------------------------------- court data ---------------------------------
 # 澳門東亞運動會體育館 
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('1', '澳門東亞運動會體育館', 'A', '0');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('1', '澳門東亞運動會體育館', 'B', '0');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('1', '澳門東亞運動會體育館', 'C', '0');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('1', '澳門東亞運動會體育館', 'D', '0');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('1', '澳門東亞運動會體育館', 'E', '1');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('1', '澳門東亞運動會體育館', 'F', '1');
-
 
 # 氹仔東北體育中心
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('2', '氹仔東北體育中心', 'I', '0');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('2', '氹仔東北體育中心', 'II', '0');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('2', '氹仔東北體育中心', 'III', '0');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('2', '氹仔東北體育中心', 'IV', '1');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('2', '氹仔東北體育中心', 'V', '1');
 
 # 奧林匹克體育中心
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '1', '1');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '2', '1');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '3', '1');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '4', '1');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '5', '1');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '6', '0');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '7', '0');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '8', '0');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '9', '0');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '10', '0');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('3', '奧林匹克體育中心(羽毛球場) OC', '11', '0');
 
 # 何東中葡小學綜合運動室
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('4', '何東中葡小學綜合運動室', '1', '0');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('4', '何東中葡小學綜合運動室', '2', '0');
 INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('4', '何東中葡小學綜合運動室', '3', '1');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('4', '何東中葡小學綜合運動室', '4', '1');
 
 # 澳大綜合體育館
-# no data
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('5', '澳大綜合體育館', '1', '0');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('5', '澳大綜合體育館', '2', '0');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('5', '澳大綜合體育館', '3', '1');
+INSERT INTO sport_ticket.tb_court (sid, display_name, court_no, is_reserved) VALUES ('5', '澳大綜合體育館', '4', '1');
 #------------------------------------------------------------------------------
